629b35f8c7291e764783dffdc927df15a84926b8
Tavo López Hernández <61797363+ovatlh@users.noreply.github.com>
2025-06-21 20:32:28 -0600

    crypto.randomUUID

--- dist/v0.1.0/tmodals.js
+++ dist/v0.1.0/tmodals.js
@@ -16,13 +16,13 @@ var tmodals = tmodals || (function () {
   };
 
   function fnGenerateId() {
-    // return crypto.randomUUID();
+    return crypto.randomUUID();
 
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-      const r = Math.random() * 16 | 0;
-      const v = c === 'x' ? r : (r & 0x3 | 0x8);
-      return v.toString(16);
-    });
+    // return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    //   const r = Math.random() * 16 | 0;
+    //   const v = c === 'x' ? r : (r & 0x3 | 0x8);
+    //   return v.toString(16);
+    // });
   }
 
   function _fnRefreshMainContainerDOM() {
